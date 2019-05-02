bf4136fe7165ac521962fc8c6672bb889f8afcf8
Levi Martines <levi.mferreira@gmail.com>
2019-05-02 19:14:15 -0300

    Observer design pattern to update tableview && Validation exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,9 +1,14 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
@@ -15,14 +20,17 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
 
 	private Department entity;
-	
+
 	private DepartmentService service;
-	
+
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+
 	@FXML
 	private TextField txtId;
 
@@ -41,35 +49,58 @@ public class DepartmentFormController implements Initializable {
 	public void setDepartment(Department entity) {
 		this.entity = entity;
 	}
-	
+
 	public void setDepartmentService(DepartmentService service) {
-		this.service=service;
+		this.service = service;
+	}
+
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
 	}
 
 	public void onBtSaveAction(ActionEvent event) {
-		
+
 		try {
-			if(entity == null) {
+			if (entity == null) {
 				throw new IllegalStateException("Entity was null");
 			}
-			if(service == null) {
+			if (service == null) {
 				throw new IllegalStateException("Service was null");
 			}
-			entity= getFormData();
+			entity = getFormData();
 			service.saveOrUpdate(entity);
+
+			// notifica os Listeners cadastrados
+			notifyDataChangeListeners();
+
 			Utils.currentStage(event).close();
-			
-		}
-		catch (DbException e) {
+		} catch (DbException e) {
 			Alerts.showAlert("Error saving object", null, e.getLocalizedMessage(), AlertType.ERROR);
+		} catch (ValidationException e) {
+			setErrorMessages(e.getErrors());
+		}
+		
+	}
+
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChanged();
 		}
 	}
 
 	private Department getFormData() {
 		Department obj = new Department();
+
+		ValidationException exception = new ValidationException("Validation error");
+
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addError("name", "Name can't be empyt");
+		}
 		obj.setName(txtName.getText());
-		
+		if (exception.getErrors().size()>0) {
+			throw exception;
+		}
 		return obj;
 	}
 
@@ -88,10 +119,17 @@ public class DepartmentFormController implements Initializable {
 	}
 
 	public void updateFormData() {
-		if(entity==null) {
+		if (entity == null) {
 			throw new IllegalStateException("Entity was null");
 		}
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
+	}
+	
+	public void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		if(fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
+		}	
 	}
 }
